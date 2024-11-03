305af599a01e20e51d0f4430212e22682019cb8d
GBlecher <gilanblecher@gmail.com>
2024-11-03 14:58:01 +0200

    first commit

---
+++ Week1/Day1/Exercises_XP/XP_Ex.py
@@ -0,0 +1,60 @@
+# Exercise 1 : Hello World
+
+print("Hello World \n" * 4)
+
+
+# Exercise 2 : Some Math
+
+answer = (99**3)*8
+print(answer)
+
+
+# Exercise 3 : What is the output ?
+
+# False
+# True
+# False
+# Error
+# False
+
+
+# Exercise 4 : Your computer brand
+
+computer_brand = "Asus"
+print(f"I have an {computer_brand} computer")
+
+
+#  Exercise 5 : Your information
+
+name = "Gilan"
+age = 27
+shoe_size = 50
+info = f"My name is {name}, I am {age} years old, and I have size {shoe_size} shoes."
+print(info)
+
+
+#Exercise 6 : A & B
+
+a = 39
+b = 7
+ 
+if a > b :
+    print("Hello World")
+
+#  Exercise 8 : What’s your name ?
+
+My_name = "Gilan"
+Your_name = input("Whats your name?")
+
+if My_name == Your_name:
+    print("If we ever meet, we should just pretend to be each other; it might confuse everyone!")
+
+
+# Exercise 9 : Tall enough to ride a roller coaster
+
+height = int(input("How tall are you?"))
+if height >  145:
+    print("You are tall enough to ride!!!")
+
+else:
+    print("You need to grow some more!!!")
