6116d154ebfe85edbdd494085fbf155265ea4706
GBlecher <gilanblecher@gmail.com>
2024-11-12 10:14:28 +0200

    WEEK2,D2 - daily challenge update

--- Week2/Day2/Daily_Challenges/D_C.py
+++ Week2/Day2/Daily_Challenges/D_C.py
@@ -1,10 +1,3 @@
-# Create a class to handle paginated content in a website. A pagination is used to divide long lists of content in a series of pages.
-
-# The Pagination class will accept 2 parameters:
-
-# items (default: None): It will contain a list of contents to paginate.
-# pageSize (default: 10): The amount of items to show in each page.
-# So for example we could initialize our pagination like this:
 
 
 class Pagination:
@@ -12,69 +5,53 @@ class Pagination:
         self.items = items
         self.pageSize = pageSize
         self.currentPage = pageSize
-        self.totalPages = (len(self.items)/self.pageSize ) + 1
+        self.totalPages = (len(self.items)/self.pageSize) + 1
 
     def getVisibleItems(self):
-
-        # return self.items[self.currentPage - self.pageSize: self.currentPage]
-        print (self.items[self.currentPage - self.pageSize: self.currentPage])
+        print(self.items[self.currentPage - self.pageSize: self.currentPage])
 
     def firstPage(self):
         self.currentPage = self.pageSize
+        self.getVisibleItems()
+        return self
 
     def nextPage(self):
         if self.currentPage < len(self.items):
             self.currentPage += self.pageSize
-
-        return self.currentPage
+        self.getVisibleItems()
+        return self
 
     def prevPage(self):
         if self.currentPage > self.pageSize:
             self.currentPage -= self.pageSize
-
-        return self.currentPage
+        self.getVisibleItems()
+        return self
 
     def lastPage(self):
-        
+
         if self.totalPages % 1 != 0:
-            lastpage = int(self.totalPages)* self.pageSize  
+            lastpage = int(self.totalPages) * self.pageSize
         else:
-            lastpage  = len(self.items)    
-        
-        self.currentPage = lastpage    
-        return self.currentPage
+            lastpage = len(self.items)
+
+        self.currentPage = lastpage
+        self.getVisibleItems()
+        return self
 
     def goToPage(self, pageNum):
-        
+
         if pageNum > int(self.totalPages):
-           self.currentPage = int(self.totalPages)* self.pageSize
-        else: 
+            self.currentPage = int(self.totalPages) * self.pageSize
+        else:
             self.currentPage = pageNum * self.pageSize
 
-     
-        return self.currentPage
-        
+        self.getVisibleItems()
+        return self
 
 
 alphabetList = list("abcdefghijklmnopqrstuvwxyz")
 
-p = Pagination(alphabetList, 11)
+p = Pagination(alphabetList, 4)
 
 
-p.firstPage()
-p.getVisibleItems()
-p.nextPage()
-p.getVisibleItems()
-p.nextPage()
-p.getVisibleItems()
-p.prevPage()
-p.getVisibleItems()
-p.firstPage()
-p.getVisibleItems()
-p.lastPage()
-p.getVisibleItems()
-p.goToPage(1)
-p.getVisibleItems()
-p.goToPage(4)
-p.getVisibleItems()
-
+p.firstPage().nextPage().nextPage().goToPage(4).lastPage().prevPage()
